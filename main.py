e497ccae772ca10687e2d49d92663cc172be7e2f
franklintimoteo <franklintimoteo@live.com>
2019-08-10 22:46:24 -0300

    recriando bot com comandos uteis

--- main.py
+++ main.py
@@ -1,85 +1,78 @@
-#!/usr/bin/python3
+import os
+import logging
 
-from os import environ
-import logging 
-import argparse
-from functools import lru_cache
-from time import time
-from requests.exceptions import ReadTimeout
+log = logging.getLogger(__name__)
 
 import telebot
-from database import user_exist, user_amount_alerts, new_alert, reset_alerts
-# TODO: configura level debug atráves do argparse
-#
-logger = logging.getLogger('main')
+from telebot import util
 
 # busca o token na variavel de ambiente
-TOKEN = environ['BOT_KEY']
-# máxio de alertas antes de banir
-MAX_ALERTS = 5
-# tempo de banir
-BAN_SECS = 86400 * 7 # 1 dia = 86400 segundos
-# afirma que token deve existir
-assert TOKEN != None, "Defina a variavel de ambiente: export BOT_KEY='{}'"
-# obtem uma instancia usando o token
-bot = telebot.TeleBot(TOKEN, skip_pending=True)
+TOKEN = os.environ['BOT_KEY']
+bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['help'])
-def send_help(msg):
-    logger.debug('Received message from user: %d' %msg.from_user.id)
-    bot.send_message(msg.chat.id, 'Helper message Working...')
-    
-@bot.message_handler(content_types=['new_chat_members'])
-def handle_new_member(msg):
-    banner = "Seja bem vindo, {}!\
-    Regras: undeadsec.github.io"
-    bot.send_message(msg.chat.id, banner.format(msg.new_chat_member.first_name))
 
-@bot.message_handler(func=lambda msg: msg.chat.type in 'supergroup' and not is_admin(msg.from_user.id, msg.chat.id), content_types=['sticker'])
-def scan_messages(msg):
-    logger.debug("'Scaniando' mensagem")
-    
-    new_alert(msg.from_user.id) # adiciona 1 alerta pela infração
-    user_alerts = user_amount_alerts(msg.from_user.id)
-        
-    if user_alerts >= MAX_ALERTS:
-        bot.reply_to(msg, 'Banido por excesso de stickers!')
-        until_date = time() + BAN_SECS
-        bot.kick_chat_member(msg.chat.id, msg.from_user.id, until_date=until_date)
-        reset_alerts(msg.from_user.id)
-    
-    elif user_alerts == MAX_ALERTS - 1:
-        bot.reply_to(msg, '%s você será banido por uma semana na próxima vez que enviar um sticker!'%msg.from_user.first_name)
+@bot.message_handler(commands=['help', 'Help'])
+def send_welcome(message):
+    if message.chat.type == 'supergroup' or message.chat.type == 'group':
+        bot.reply_to(message, '''**UNDEADBOT**
+
+BASIC
+/help : to access help menu
+/rules : to see rules of the group       
+
+TOOLS
+/ping : to do ping on hosts
+/testip 8.8.8.8: to see if an IP is on
+/whois : to do whois check        
+        ''')
+def is_private(message):
+        """verifica se o chat é privado
+        param: obj telebot message
+        return: bool"""
+        log.info('Checking type chat...')
+        return message.chat.type == 'private'
+
+@bot.message_handler(commands=['rules', 'Rules'])
+def send_rules(message):
+    # verifica se o chat é um group ou supergrop
+    if message.chat.type in 'supergroup':
+        bot.reply_to(message, "Look at our rules in undeadsec.github.com")
+
+
+@bot.message_handler(func=is_private, commands=['ping', 'Ping'])
+def verify_ip_online(message):
+    # extrai o argumento que deve ser o ip
+    ip = util.extract_arguments(message.text)
+    log.debug('Verifing if ip %r is online', ip)
+    # executa o ping e verfica o código de retorno
+    result = os.system("ping -c1 -W 1 %s > /dev/null" %ip)
+    # TODO: usar regex para filtrar o ip evitando command inject
+    if result == 0:
+        bot.reply_to(message, "%s is working !" %ip)
     else:
-        bot.reply_to(msg, '%s é proibido enviar stickers nesse grupo. Notificação %d/%d' %(msg.from_user.first_name, user_alerts, MAX_ALERTS))
+        bot.reply_to(message, "%s is not working !" %ip)  
 
 
-def is_admin(uid, cid):
-    admins = get_admins_from_chat(cid)
-    return uid in admins
+@bot.message_handler(func=is_private, commands=['whois'])
+def execute_whois(message):
+    if message.chat.type in 'supergroup': # Filtra group e supergroup
+        message_received = message.text.split()
+        r = os.system("whois {} > results/whois.txt".format(message_received[1]))
+        result = open('results/whois.txt', 'r')
+        bot.reply_to(message, ping_result)                 
 
-def get_admins_from_chat(cid):
-    _admins = bot.get_chat_administrators(cid)
-    return [admin.user.id for admin in _admins]
 
-def configure_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--debug', help='Nivel do debug, (INT|STRING)')
-    parser.add_argument('-o', '--output', type=str, help='Arquivo para saida')
-    
-    args = parser.parse_args()
-    # configura level do debug
-    level = 'NOTSET' if not args.debug else args.debug
-    logger.setLevel(level)
-    
-    # especifica formato de saida
-    FORMAT = "%(asctime)-15s %(name)-8s %(message)s'"
-    filename = None if not args.output else args.output
-    logging.basicConfig(format=FORMAT, filename=filename, filemode='w')
-    logger.debug('Parser configurado!')
-    
+@bot.message_handler(content_types=['new_chat_members'])
+def send_welcome_new_members(message):
+    bot.reply_to(message, "Hey, welcome to UndeadSec !\nType /rules to see the rules.")   
+
+
 if __name__ == '__main__':
-    configure_parser()
-    # TODO: não está salvando log no arquivo
+    # ignora logs do urllib3
+    logging.getLogger('urllib3').setLevel(logging.CRITICAL)
+
+    # configura logging para debug
+    logging.basicConfig(level=logging.DEBUG)
+    
     print('running...')
-    bot.polling(none_stop=True, interval=5, timeout=600)
+    bot.polling()
